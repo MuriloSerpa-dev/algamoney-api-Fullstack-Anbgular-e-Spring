8fdf6df7a9d353ded9272687a7f7d67b372739b1
MuriloSerpa-dev <muriloserpa2@gmail.com>
2023-07-04 15:23:40 -0300

    3.6 Cadastrando Nova Categoria com POST

--- src/main/java/com/algaworks/algamoney/api/repository/CategoriaRepository.java
+++ src/main/java/com/algaworks/algamoney/api/repository/CategoriaRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     // Criamos a categoria de interface que extends JpaRepository que ja vem com varios metodos
     // e a implementação quem vai da SpringDataJpa que com isso ja ganhamos varios metodos
-    // colocamos esses parametros que é preciso dizer pra ele que é pra trabalhar em cima do metodo
+    // colocamos esses parametros que é preciso dizer pra ele que é pra trabalhar em cima do metodo e
    // configuramos o tipo da chave primaria
 }
--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -2,14 +2,19 @@ package com.algaworks.algamoney.api.resource;
 
 import com.algaworks.algamoney.api.model.Categoria;
 import com.algaworks.algamoney.api.repository.CategoriaRepository;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController // Usado para dizermos que somos um controlador Rest
 @RequestMapping("/categorias") // fazendo o mapeamento da aplicação, quando buscarmos categoria esse metodo é que
@@ -23,11 +28,31 @@ public class CategoriaResource {
     public List<Categoria> Listar() {
         return categoriaRepository.findAll(); // findAll quer é como se fizessemos um select from
     }
-
         // return !categorias.isEmpty()?  ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
         // ResponseEntity é uma entidade de resposta que posso retornar e interrrogação porque não esta deficinada
         // a resposta sera dependente do que estiver vindo na lista de categorias
         // entity se a lista estiver vazia
         // No caso se ela não estiver vazia retorno 200 OK passando as categorias
         // NO caso de estar vazia o retorno sera de 404NotFound build é preciso seer colocado pra poder gerar um response entity
+
+        @PostMapping
+        // quero que retorne um http status 201Created
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){ // RequestBody ja consegue pegar, a nova categoria criar
+        // e transformar em um objeto
+       Categoria categoriaSalva = categoriaRepository.save(categoria); // para salvar no banco de dados usamos
+            // usamos a interface com os metodos e usamos o metodo SAVE e passamos a entidade, que objeto deve ser salvo
+           URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                    .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+           response.setHeader("Location", uri.toASCIIString());
+            // Quando eu crio algum recruso no banco de dados, o rest fala que devemos voltar no headres de resposta
+            // para informar como recuperar esse recurso que foi criado no banco de dados
+            // Esse metodo e feito para um request ou quando um novo recurso foi criado e para recuperar esse
+            // recurso criado
+            return ResponseEntity.created(uri).body(categoriaSalva);
+            // Definindo o status
+        }
+        @GetMapping("/{codigo}")
+        public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo){
+            return categoriaRepository.findById(codigo);
+        }
 }
